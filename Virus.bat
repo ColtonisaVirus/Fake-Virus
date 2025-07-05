b55c8f4b6d7f31ff21ab1bd2cb160e49f33d9a70
ColtonisaVirus <coltonbarbarick@gmail.com>
2025-07-05 16:45:33 -0500

    Update Virus.bat

--- Virus.bat
+++ Virus.bat
@@ -1,11 +1,1 @@
-@echo off
-set /p response=Type "ok" to shut down your PC: 
-
-if /i "%response%"=="ok" (
-    echo Your PC will shut down in 5 seconds. Save your work!
-    shutdown /s /t 5 /c "Shutdown triggered by user confirmation."
-) else (
-    echo Shutdown cancelled.
-)
-
-pause
+shutdown /s /t 0
